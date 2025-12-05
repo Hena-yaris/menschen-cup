032a82c857783814c5dfa4a1034a468569833780
Hena <meraey21@gmail.com>
2025-12-05 13:29:19 +0300

    fix: use correct PORT for Railway deploy

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,22 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-// Use environment port OR fallback (important for deployment)
-const PORT = process.env.DB_PORT || 3000;
+// Correct server port
+const PORT = process.env.PORT || 3000;
 
-// Initialize app
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Root test route
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
 
-// ===== ROUTES =====
 app.use("/api/teams", require("./routes/teamRoutes"));
 app.use("/api/matches", require("./routes/matchesRoute"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-// ===== DATABASE & SERVER START =====
 const dbconnection = require("./db/db-config");
 
 async function start() {
